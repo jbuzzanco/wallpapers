955a272e707ce5b605831cc935a553148be6d06a
Joel on Design <joel@Joels-Mac-mini.local>
2023-06-12 18:03:09 -0400

    Sets wallpaper to local file path, next steps would be to clean up the UI, design some wallpapers.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,44 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 1000,
     webPreferences: {
       nodeIntegration: true,
+      contextIsolation: false,
+      preload: path.join(__dirname, 'preload.js'),
     },
   });
 
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 }
 
-app.whenReady().then(createWindow);
+app.on('ready', () => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+// Listen for 'set-wallpaper' message from renderer process
+ipcMain.on('set-wallpaper', (event, imagePath) => {
+  // Call the function to set the desktop wallpaper
+  setWallpaper(imagePath);
+});
+
+// Function to set the desktop wallpaper
+async function setWallpaper(imagePath) {
+  try {
+    const { setWallpaper } = await import('wallpaper');
+    const directoryPath = path.join(__dirname, 'assets', 'images');
+    const fullPath = path.join(directoryPath, imagePath.replace('assets/images/', ''));
+    console.log('Full path:', fullPath); // Log the full path for troubleshooting
+    await setWallpaper(fullPath);
+    console.log('Wallpaper set successfully');
+  } catch (error) {
+    console.error('Error setting wallpaper:', error);
+  }
+}

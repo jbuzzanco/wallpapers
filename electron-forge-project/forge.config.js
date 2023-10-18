7c32a7810c106ec644f64f0aa0cefbe68bef76c3
Joel on Design <joel@Joels-Mac-mini.local>
2023-10-18 19:11:55 -0400

    Removes unnecessary  packages from the electron-forge-project. removes them from the package.json and forge.config.js files.

--- electron-forge-project/forge.config.js
+++ electron-forge-project/forge.config.js
@@ -11,29 +11,8 @@ module.exports = {
   },
   "electronPackagerConfig": {
     "electron": "25.3.0",
-    "osxSign": {
-      "identity": "Developer ID Application: Joelondesign Dev Key",
-    }
   },
   rebuildConfig: {},
-  makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
-  ],
   plugins: [
     {
       name: '@electron-forge/plugin-auto-unpack-natives',

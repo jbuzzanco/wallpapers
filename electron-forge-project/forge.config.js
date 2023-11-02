000079831f61f8e98e49120d29848d6e1f326a29
Joel on Design <joel@Joels-Mac-mini.local>
2023-11-02 15:53:22 -0400

    Signs with app with Mac Distribution certificate, npx run sign, and intermediary certificate installed before running npx run sign

--- electron-forge-project/forge.config.js
+++ electron-forge-project/forge.config.js
@@ -5,13 +5,22 @@ module.exports = {
     asar: true,
      // Add the code signing configuration
      osxSign: {
-      identity: 'JoelonDesign Distribution Key',
+      identity: "D9D9E1B04A93458ECC39BB41E50C55472E06D1D9",
     },
     icon: path.join(__dirname, 'src', 'icons', 'mac', 'testflight.icns'),
   },
   "electronPackagerConfig": {
     "electron": "25.3.0",
   },
+  makers: [
+    {
+      name: '@electron-forge/maker-pkg',
+      config: {
+        bundleIdentifier: 'com.joelondesign.wallpapers',
+        identity: '3rd Party Mac Developer Application: Joel Buzzanco (J5HUPF6M97)',
+      },
+    },
+  ],
   rebuildConfig: {},
   plugins: [
     {
